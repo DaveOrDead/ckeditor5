934c282f4f513c4e02844b808be5490ea3b36ff3
daveordead <davidajberner@gmail.com>
2020-12-08 00:30:06 +1100

    remove unused plugins

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -9,24 +9,14 @@ import InlineEditorBase from "@ckeditor/ckeditor5-editor-inline/src/inlineeditor
 
 import Autosave from "@ckeditor/ckeditor5-autosave/src/autosave";
 import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
-import UploadAdapter from "@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter";
 import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
 import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
 import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
-import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
-import CKFinder from "@ckeditor/ckeditor5-ckfinder/src/ckfinder";
-import EasyImage from "@ckeditor/ckeditor5-easy-image/src/easyimage";
 import Heading from "@ckeditor/ckeditor5-heading/src/heading";
 import Highlight from "@ckeditor/ckeditor5-highlight/src/highlight";
-import Image from "@ckeditor/ckeditor5-image/src/image";
-import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption";
-import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle";
-import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar";
-import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload";
 import Indent from "@ckeditor/ckeditor5-indent/src/indent";
 import Link from "@ckeditor/ckeditor5-link/src/link";
 import List from "@ckeditor/ckeditor5-list/src/list";
-import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed";
 import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
 import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
 import Placeholder from "../plugins/tokens";
@@ -43,24 +33,13 @@ class InlineEditor extends InlineEditorBase {}
 // Plugins to include in the build.
 const plugins = [
 	Essentials,
-	UploadAdapter,
-	Autoformat,
 	Bold,
 	Italic,
-	BlockQuote,
-	CKFinder,
-	EasyImage,
 	Heading,
 	Highlight,
-	Image,
-	ImageCaption,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload,
 	Indent,
 	Link,
 	List,
-	MediaEmbed,
 	Paragraph,
 	PasteFromOffice,
 	Placeholder,
@@ -89,22 +68,9 @@ const config = {
 			"indent",
 			"outdent",
 			"|",
-			"imageUpload",
-			"blockQuote",
 			"insertTable",
-			"mediaEmbed",
-			"undo",
-			"redo",
 			"|",
 			"placeholder",
-		],
-	},
-	image: {
-		toolbar: [
-			"imageStyle:full",
-			"imageStyle:side",
-			"|",
-			"imageTextAlternative",
 		],
 	},
 	table: {
